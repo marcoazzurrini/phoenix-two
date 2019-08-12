d349ab4e34aa482dde28b7502c5d35014c7665d5
marcoazzurrini <marco.azzurrini.art@gmail.com>
2019-08-12 11:25:19 +0200

    captcha

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/contact.html');
 });
 
 app.post('/subscribe', (req, res) => {
